89db5b79470a04f35b295b90df69cfbba5914084
By Daniel Luna <daniluna7529@gmail.com>
2025-02-25 14:15:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Cambia la imagen
+    const imagenDengue = document.getElementById('imagenDengue');
+
+   
+    const imagen1 = 'https://agn.gt/wp-content/uploads/2024/03/Recomendaciones-para-prevenir-el-dengue.-e1710350107227.jpg';
+    const imagen2 = 'https://www.salud.gob.sv/wp-content/uploads/2022/08/como-se-trasmite-el-dengue-cuidemonos-evitemos-el-dengue-800x400-1.jpg';
+
+    
+    function cambiarImagen() {
+        if (imagenDengue.src === imagen1) {
+            imagenDengue.src = imagen2;
+            imagenDengue.alt = 'Transmisión del Dengue';
+        } else {
+            imagenDengue.src = imagen1; // Cambia a la primera imagen
+            imagenDengue.alt = 'Prevención del Dengue';
+        }
+    }
+
+  
+    imagenDengue.addEventListener('click', cambiarImagen);
+});
